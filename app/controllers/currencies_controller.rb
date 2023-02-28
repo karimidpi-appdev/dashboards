202e0edf90181319c9769c61ee163f939eb99086
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-28 02:15:50 +0000

    getting converting rate

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -23,4 +23,22 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_two.html.erb"})
   end
 
+  def third_currency
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+    
+    @currency_api_url = open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}").read
+    @parsed_convert = JSON.parse(@currency_api_url)
+    @rate = @parsed_convert.fetch("info").fetch("rate")
+
+    render({ :template => "currency_templates/step_three.html.erb"})
+  end
+
 end
